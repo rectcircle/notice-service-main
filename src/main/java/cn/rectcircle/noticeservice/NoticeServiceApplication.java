7b4581669188cf5be4fb86e8e84a0e14d46a949e
Rectcircle <sunben960729@163.com>
2018-06-03 21:03:18 +0800

    最终初始化提交

--- src/main/java/cn/rectcircle/noticeservice/NoticeServiceApplication.java
+++ src/main/java/cn/rectcircle/noticeservice/NoticeServiceApplication.java
@@ -3,22 +3,14 @@ package cn.rectcircle.noticeservice;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class DemoApplication {
+public class NoticeServiceApplication {
 
 	@Value("${spring.datasource.password}")
 	private String msg;
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
-
-	@Bean
-	public String justATeset() {
-		System.out.println("====================================================================================="+msg);
-		return msg;
+		SpringApplication.run(NoticeServiceApplication.class, args);
 	}
 }
