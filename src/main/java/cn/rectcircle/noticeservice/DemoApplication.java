1b1255c09b0a80a672f733392ccdf260e9dc66ee
Rectcircle <sunben960729@163.com>
2018-06-03 19:24:22 +0800

    测试

--- src/main/java/cn/rectcircle/noticeservice/DemoApplication.java
+++ src/main/java/cn/rectcircle/noticeservice/DemoApplication.java
@@ -1,12 +1,24 @@
 package cn.rectcircle.noticeservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@Value("${spring.datasource.password}")
+	private String msg;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+
+	@Bean
+	public String justATeset() {
+		System.out.println("====================================================================================="+msg);
+		return msg;
 	}
 }
